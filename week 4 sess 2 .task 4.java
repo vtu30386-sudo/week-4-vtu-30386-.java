86a897af8c369ce983289b2ae0d48d76af192474
vtu30386-sudo <vtu30386@veltech.edu.in>
2026-08-28 09:22:56 +0530

    Add files via upload

---
+++ week 4 sess 2 .task 4.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+
+        return false;
+    }
+}
+
+output:
+Input
+nums =
+[1,2,3,1]
+Output
+true
+Expected
+true
